cac483cfc3fb0283da6b7a68ca6349d0b08445e9
anonymze <anodevfr@gmail.com>
2025-09-21 23:31:35 +0200

    add: api route

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://prix-site-internet.fr",
+  site: "https://www.prix-site-internet.fr",
   // experimental: {
   //   fonts: [
   //     {
